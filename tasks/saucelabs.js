263a6f5cbc8b3ea1112192be5b281cf618152915
Justin Reidy <jmreidy@rzrsharp.net>
2013-05-20 13:42:18 -0700

    Move dependency on SauceTunnel to a separate module

--- tasks/saucelabs.js
+++ tasks/saucelabs.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
   var _ = (grunt.utils || grunt.util)._,
     request = require('request'),
-    proc = require('child_process'),
     wd = require('wd'),
+    SauceTunnel = require('sauce-tunnel'),
     rqst = request.defaults({
       jar: false
     });
@@ -43,122 +43,6 @@ module.exports = function(grunt) {
       body: _body,
       json: true
     }, function() {
-      callback();
-    });
-  };
-
-  var SauceTunnel = function(user, key, identifier, tunneled, tunnelTimeout) {
-    this.user = user;
-    this.key = key;
-    this.identifier = identifier;
-    this.tunneled = tunneled;
-    this.tunnelTimeout = tunnelTimeout;
-    this.baseUrl = ["https://", this.user, ':', this.key, '@saucelabs.com', '/rest/v1/', this.user].join("");
-  };
-
-  SauceTunnel.prototype.openTunnel = function(callback) {
-    var args = ["-jar", __dirname + "/Sauce-Connect.jar", this.user, this.key, "-i", this.identifier];
-    this.proc = proc.spawn('java', args);
-    var calledBack = false;
-
-    this.proc.stdout.on('data', function(d) {
-      var data = typeof d !== 'undefined' ? d.toString() : '';
-      if (typeof data === 'string' && !data.match(/^\[-u,/g)) {
-        grunt.verbose.debug(data.replace(/[\n\r]/g, ''));
-      }
-      if (typeof data === 'string' && data.match(/Connected\! You may start your tests/)) {
-        grunt.verbose.ok('=> Sauce Labs Tunnel established');
-        if (!calledBack) {
-          calledBack = true;
-          callback(true);
-        }
-      }
-    });
-
-    this.proc.stderr.on('data', function(data) {
-      grunt.log.error(data.toString().replace(/[\n\r]/g, ''));
-    });
-
-    this.proc.on('exit', function(code) {
-      grunt.verbose.ok('Sauce Labs Tunnel disconnected ', code);
-      if (!calledBack) {
-        calledBack = true;
-        callback(false);
-      }
-    });
-  };
-
-  SauceTunnel.prototype.getTunnels = function(callback) {
-    rqst({
-      url: this.baseUrl + '/tunnels',
-      json: true
-    }, function(err, resp, body) {
-      callback(body);
-    });
-  };
-
-  SauceTunnel.prototype.killAllTunnels = function(callback) {
-    if (!this.tunneled) {
-      return callback();
-    }
-    var me = this;
-    grunt.verbose.debug("Trying to kill all tunnels");
-    this.getTunnels(function(tunnels) {
-      (function killTunnel(i) {
-        if (i >= tunnels.length) {
-          setTimeout(callback, 1000 * 5);
-          return;
-        }
-        grunt.log.writeln("=> Killing tunnel %s", tunnels[i]);
-        rqst({
-          method: "DELETE",
-          url: me.baseUrl + "/tunnels/" + tunnels[i],
-          json: true
-        }, function() {
-          killTunnel(i + 1);
-        });
-      }(0));
-    });
-  };
-
-  SauceTunnel.prototype.start = function(callback) {
-    var me = this;
-    if (!this.tunneled) {
-      return callback(true);
-    }
-    this.getTunnels(function(tunnels) {
-      if (!tunnels) {
-        grunt.verbose.error("=> Could not get tunnels for Sauce Labs. Still continuing to try connecting to Sauce Labs".inverse);
-      }
-      if (tunnels && tunnels.length > 0) {
-        grunt.log.writeln("=> Looks like there are existing tunnels to Sauce Labs, need to kill them. TunnelID:%s", tunnels);
-        (function waitForTunnelsToDie(retryCount) {
-          if (retryCount > 5) {
-            grunt.verbose.writeln("=> Waited for %s retries, now trying to shut down all tunnels and try again", retryCount);
-            me.killAllTunnels(function() {
-              me.start(callback);
-            });
-          } else {
-            grunt.verbose.debug("=> %s. Sauce Labs tunnels already exist, will try to connect again %s milliseconds.", retryCount, me.tunnelTimeout / 5);
-            setTimeout(function() {
-              waitForTunnelsToDie(retryCount + 1);
-            }, me.tunnelTimeout / 5);
-          }
-        }(0));
-      } else {
-        grunt.verbose.writeln("=> Sauce Labs trying to open tunnel".inverse);
-        me.openTunnel(function(status) {
-          callback(status);
-        });
-      }
-    });
-  };
-
-  SauceTunnel.prototype.stop = function(callback) {
-    if (this.proc) {
-      this.proc.kill();
-    }
-    this.killAllTunnels(function() {
       callback();
     });
   };
@@ -698,10 +582,23 @@ module.exports = function(grunt) {
     return result;
   }
 
+  function configureLogEvents(tunnel) {
+    var methods = ['write', 'writeln', 'error', 'ok', 'debug'];
+    methods.forEach(function (method) {
+      tunnel.on('log:'+method, function (text) {
+        grunt.log[method](text);
+      });
+      tunnel.on('verbose:'+method, function (text) {
+        grunt.verbose[method](text);
+      });
+    });
+  }
+
   grunt.registerMultiTask('saucelabs-jasmine', 'Run Jasmine test cases using Sauce Labs browsers', function() {
     var done = this.async(),
       arg = defaults(this.options(defaultsObj), this.data.browsers);
     var tunnel = new SauceTunnel(arg.username, arg.key, arg.identifier, arg.tunneled, arg.tunnelTimeout);
+    configureLogEvents(tunnel);
     grunt.log.writeln("=> Connecting to Saucelabs ...");
     if (this.tunneled) {
       grunt.verbose.writeln("=> Starting Tunnel to Sauce Labs".inverse.bold);
@@ -726,6 +623,7 @@ module.exports = function(grunt) {
     var done = this.async(),
       arg = defaults(this.options(defaultsObj));
     var tunnel = new SauceTunnel(arg.username, arg.key, arg.identifier, arg.tunneled, arg.tunnelTimeout);
+    configureLogEvents(tunnel);
     grunt.log.writeln("=> Connecting to Saucelabs ...");
     if (this.tunneled) {
       grunt.verbose.writeln("=> Starting Tunnel to Sauce Labs".inverse.bold);
