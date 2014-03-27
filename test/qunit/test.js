1601e96e713ba94641d8a8b4dd22b02d0074ec6b
Jonah <jonahss@gmail.com>
2014-03-27 15:34:24 -0700

    Update test.js
    update reporting code.

--- test/qunit/test.js
+++ test/qunit/test.js
@@ -823,9 +823,7 @@ QUnit.done = function (test_results) {
   });
   test_results.tests = tests;
 
-  // delaying results a bit cause in real-world
-  // scenario you won't get them immediately
-  setTimeout(function () { window.global_test_results = test_results; }, 2000);
+  window.global_test_results = test_results; };
 };
 
 QUnit.testStart(function(testDetails){
